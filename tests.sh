9d93c005e54f7c5e4f6bed80c43b7171992182f5
Lakshmi Narasimhan <lakshmi@shapeblock.com>
2019-07-05 12:33:09 +0530

    Add composer to tests

--- tests.sh
+++ tests.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 export SIMPLETEST_DB="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE}"
 export SIMPLETEST_BASE_URL="http://drupal-test-1-drupal-test.apps.c2151-de.firelay.io"
+composer install
 cd /app/web/core && ../../vendor/bin/phpunit modules/system/tests/src/Kernel/Extension/ModuleHandlerTest.php
